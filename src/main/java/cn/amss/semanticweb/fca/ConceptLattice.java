f7e9d0c8b55378b419aa1c1bc56be4866010326a
icgw <icgw@outlook.com>
2019-09-18 20:04:33 +0800

    add fundamental feature for ConceptLattice (wait to test)

--- src/main/java/cn/amss/semanticweb/fca/ConceptLattice.java
+++ src/main/java/cn/amss/semanticweb/fca/ConceptLattice.java
@@ -16,6 +16,9 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Arrays;
+import java.util.TreeSet;
+import java.util.Comparator;
+import java.util.Iterator;
 
 /**
  * A concept lattice.
@@ -24,32 +27,39 @@ import java.util.Arrays;
  */
 public class ConceptLattice <O, A>
 {
-  Map<O, Integer> object2Id    = null;
-  Map<A, Integer> attribute2Id = null;
-
-  Set<O> topObjects       = null;
-  Set<A> bottomAttributes = null;
-
-  // Map<Concept<Integer, Integer>, Set<Concept<Integer, Integer>>> topDown  = null;
-  // Map<Concept<Integer, Integer>, Set<Concept<Integer, Integer>>> bottomUp = null;
   Map<Integer, Concept<O, A>> id2Concept = null;
   Map<Concept<O, A>, Integer> concept2Id = null;
 
   int topId    = -1;
   int bottomId = -1;
 
+  int numberOfConcepts = 0;
+
   Map<Integer, Set<Integer>> topDown  = null;
   Map<Integer, Set<Integer>> bottomUp = null;
 
-  public ConceptLattice() {
-    object2Id        = new HashMap<>();
-    attribute2Id     = new HashMap<>();
-    topObjects       = new HashSet<>();
-    bottomAttributes = new HashSet<>();
+  private ConceptLattice() {
     id2Concept       = new HashMap<>();
     concept2Id       = new HashMap<>();
     topDown          = new HashMap<>();
     bottomUp         = new HashMap<>();
+  }
+
+  public ConceptLattice(Set<Concept<O, A>> concepts) {
+    this();
+
+    Set<O> all_objects    = new HashSet<>();
+    Set<A> all_attributes = new HashSet<>();
+
+    for (Concept<O, A> c : concepts) {
+      all_objects.addAll(c.getExtent());
+      all_attributes.addAll(c.getIntent());
+    }
+
+    Concept<O, A> top    = new Concept<>(all_objects, new HashSet<>());
+    Concept<O, A> bottom = new Concept<>(new HashSet<>(), all_attributes);
+
+    init(concepts, top, bottom);
   }
 
   public ConceptLattice(Set<Concept<O, A>> concepts, Concept<O, A> top, Concept<O, A> bottom) {
@@ -58,63 +68,147 @@ public class ConceptLattice <O, A>
   }
 
   public void init(Set<Concept<O, A>> concepts, Concept<O, A> top, Concept<O, A> bottom) {
+    TreeSet<Concept<O, A>> tConcepts = new TreeSet<>(new Comparator<Concept<O, A>>() {
+      @Override
+      public int compare(Concept<O, A> a, Concept<O, A> b) {
+        Set<O> aExtent = a.getExtent();
+        Set<O> bExtent = b.getExtent();
+        if (aExtent.size() == bExtent.size()) {
+          Set<A> aIntent = a.getIntent();
+          Set<A> bIntent = b.getIntent();
+          if (aIntent.size() == bIntent.size()) {
+            return a.toString().compareTo(b.toString());
+          }
+          return aIntent.size() - bIntent.size();
+        }
+
+        return bExtent.size() - aExtent.size();
+      }
+    });
+
+    tConcepts.addAll(concepts);
+
     int idx = 0;
     for (Concept<O, A> c : concepts) {
       id2Concept.put(idx, c);
       concept2Id.put(c, idx);
       ++idx;
     }
+    numberOfConcepts = idx;
 
-    // XXX: check if top or bottom not exist in concepts.
+    // XXX: check if top or bottom not exist in concepts. (wait to improve)
     topId    = concept2Id.getOrDefault(top,    -1);
     bottomId = concept2Id.getOrDefault(bottom, -2);
   }
 
   private boolean isUpDown(Concept<O, A> up, Concept<O, A> down) {
+    if (up == null || down == null || up.equals(down)) return false;
     return up.getExtent().containsAll(down.getExtent());
   }
   private boolean isUpDown(int upId, int downId) {
+    if (upId == downId) return false;
     return isUpDown(id2Concept.get(upId), id2Concept.get(downId));
   }
 
   private boolean isDownUp(Concept<O, A> down, Concept<O, A> up) {
+    if (down == null || up == null || down.equals(up)) return false;
     return down.getIntent().containsAll(up.getIntent());
   }
 
   private boolean isDownUp(int downId, int upId) {
+    if (downId == upId) return false;
     return isDownUp(id2Concept.get(downId), id2Concept.get(upId));
   }
 
-  public void buildTopDown(Set<Concept<O, A>> concepts, Concept<O, A> top) {
+  public void buildTopDown(Set<Concept<O, A>> concepts) {
     Queue<Integer> parentIdQueue = new LinkedList<Integer>();
-    int topId                    = concept2Id.get(top);
-    for (Concept<O, A> c : concepts) {
+    for (int cId = 0; cId < numberOfConcepts; ++cId) {
+      if (cId == topId) continue;
+
       parentIdQueue.offer(topId);
-      int cId = concept2Id.get(c);
       while ( !parentIdQueue.isEmpty() ) {
         int pId                 = parentIdQueue.poll();
         Set<Integer> childrenId = topDown.get(pId);
+
         if (childrenId == null || childrenId.isEmpty()) {
           topDown.put(pId, new HashSet<Integer>(Arrays.asList(cId)));
         } else {
-          Set<Integer> childrenIdClone = new HashSet<>(childrenId);
-          for (int childId : childrenIdClone) {
+          Set<Integer> childrenIdCopy = new HashSet<>(childrenId);
+
+          boolean bHasExec = false;
+          for (int childId : childrenIdCopy) {
+            if (cId == childId) continue;
+
             if (isUpDown(cId, childId)) {
               childrenId.remove(childId);
               childrenId.add(cId);
-              topDown.get(cId).add(childId);
+              Set<Integer> cDownId = topDown.get(cId);
+              if (cDownId == null || cDownId.isEmpty()) {
+                topDown.put(cId, new HashSet<Integer>(Arrays.asList(childId)));
+              } else {
+                cDownId.add(childId);
+              }
+              bHasExec = true;
             }
             else if (isDownUp(cId, childId)) {
               parentIdQueue.offer(childId);
+              bHasExec = true;
             }
+          }
+
+          if (!bHasExec) {
+            childrenId.add(cId);
           }
         }
       }
     }
-    // TODO: need test topDown
   }
 
-  public void buildBottomUp(Set<Concept<O, A>> concepts, Concept<O, A> bottom) {
-    // TODO: bottomUp
+  public void buildBottomUp(Set<Concept<O, A>> concepts) {
+    if (topDown == null || topDown.isEmpty()) {
+      buildTopDown(concepts);
+    }
+
+    for (Map.Entry<Integer, Set<Integer>> e : topDown.entrySet()) {
+      int v = e.getKey();
+
+      for (int k : e.getValue()) {
+        Set<Integer> si = bottomUp.get(k);
+        if (si == null) {
+          bottomUp.put(k, new HashSet<>(Arrays.asList(v)));
+        } else {
+          si.add(v);
+        }
+      }
+    }
+  }
+
+  public String dotLangFormat() {
+    if (topDown == null || topDown.isEmpty()) return "graph { }";
+
+    StringBuilder dotLanguage = new StringBuilder();
+    dotLanguage.append(String.format("%s {%n", "graph"));
+
+    for (Map.Entry<Integer, Concept<O, A>> e : id2Concept.entrySet()) {
+      // XXX: we should guarantee that e.getValue().toString() not has quotation mark (")
+      dotLanguage.append(String.format("  %d [label = \"%s\"];%n", e.getKey(), e.getValue().toString()));
+    }
+
+    for (Map.Entry<Integer, Set<Integer>> e : topDown.entrySet()) {
+      dotLanguage.append(String.format("  %d -- { ", e.getKey()));
+
+      for (Iterator<Integer> it = e.getValue().iterator(); it.hasNext(); ) {
+        dotLanguage.append(String.format("%d", it.next()));
+
+        if (it.hasNext()) {
+          dotLanguage.append(", ");
+        } else {
+          dotLanguage.append(String.format(" };%n"));
+        }
+      }
+    }
+    dotLanguage.append("}");
+
+    return dotLanguage.toString();
   }
 }
